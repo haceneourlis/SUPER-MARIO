29929d9fe25f8e7dcc47f181fb8782537c7fe04b
haceneourlis <hacene.ourlis@etu-upsaclay.fr>
2025-03-20 11:33:42 +0100

    jumping uses a CONSTANT

--- src/modele/Jumping.java
+++ src/modele/Jumping.java
@@ -20,11 +20,8 @@ public class Jumping extends Thread {
     // Constante de la puissance de l'impulsion à chaque saut
     public final int IMPULSION = 15;
 
-    // Constante de la gravité
-    public final int GRAVITY = 1;
-
     // Constante du délai entre chaque vérification du saut
-    public final int DELAY = 25;
+    public final int DELAY = 32;
 
     // Constructeur qui initialise l'instance de Joueur, la force et le booléen de
     // saut.
@@ -60,7 +57,8 @@ public class Jumping extends Thread {
             if (this.is_jumping) {
                 /*
                  * On ne s'autorise pas à aller dans des valeurs au dela du sol.
-                 * Je vérifie donc que le fait de retirer de la force ne fasse pas sortir le joueur
+                 * Je vérifie donc que le fait de retirer de la force ne fasse pas sortir le
+                 * joueur
                  * de la fenêtre.
                  * Si c'est le cas, on arrête le saut, on remet la force à 0 et on remet le
                  * joueur au sol.
@@ -68,6 +66,7 @@ public class Jumping extends Thread {
                  */
                 if (this.j.getPosition().y - this.force >= CONSTANTS.LE_SOL) {
                     this.is_jumping = false;
+                    // System.out.println("are we alive here ( jumoing ?)");
                     this.force = 0;
                     this.j.setPositionY(CONSTANTS.LE_SOL);
                 }
@@ -75,7 +74,13 @@ public class Jumping extends Thread {
                 // force de la gravité.
                 else {
                     this.j.setPositionY(this.j.getPosition().y - this.force);
-                    this.force -= this.GRAVITY;
+                    this.force -= CONSTANTS.GRAVITY;
+                    // System.out.println("yes we are alive");
+                    // si force < 0 alors descente
+                    if (this.force < 0) {
+                        j.setDirection("down");
+                    }
+
                 }
 
             }
@@ -83,4 +88,9 @@ public class Jumping extends Thread {
         }
 
     }
+
+    public void notjumping() {
+        this.is_jumping = false;
+    }
+
 }
